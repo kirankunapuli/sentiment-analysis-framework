029a33ce7add38d7d9d0c7b7429e083a4c46d53f
Kiran Kunapuli V S <kirankunapuli@users.noreply.github.com>
2018-01-05 23:07:04 +0530

    Added various classification algorithms
    Added various classification algorithms and corresponding confusion matrix

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,6 @@
 # Natural Language Processing - Sentiment Analysis
 
-# Importingthe libraries
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,7 +10,7 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
 # Cleaning the texts
 import re
-import nltk
+#import nltk
 #nltk.download('stopwords', download_dir = '/Users/kirankunapuli/anaconda3/lib/nltk_data')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
@@ -31,11 +31,12 @@ X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 # Fitting Naive Bayes to the Training set
 from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import cross_val_predict
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
@@ -47,34 +48,84 @@ from sklearn.metrics import confusion_matrix, classification_report
 cm = confusion_matrix(y_test, y_pred)
 
 import scikitplot as skplt
-skplt.metrics.plot_confusion_matrix(y_test, y_pred, normalize=False)
-plt.show()
+#skplt.metrics.plot_confusion_matrix(y_test, y_pred, normalize=False, title = 'Naive Bayes')
+#plt.show()
 
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import LinearSVC
-from sklearn.neural_network import MLPClassifier
+def plot_classifications():
+    from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.svm import LinearSVC
+    from sklearn.neural_network import MLPClassifier
 
-rf = RandomForestClassifier()
-ada = AdaBoostClassifier()
-lr = LogisticRegression()
-nb = GaussianNB()
-svm = LinearSVC()
-mlp = MLPClassifier()
+    rf = RandomForestClassifier()
+    ada = AdaBoostClassifier()
+    lr = LogisticRegression()
+    nb = GaussianNB()
+    svm = LinearSVC()
+    mlp = MLPClassifier()
 
-rf_probas = rf.fit(X_train, y_train).predict_proba(X_test)
-ada_probas = ada.fit(X_train, y_train).predict_proba(X_test)
-lr_probas = lr.fit(X_train, y_train).predict_proba(X_test)
-nb_probas = nb.fit(X_train, y_train).predict_proba(X_test)
-svm_scores = svm.fit(X_train, y_train).decision_function(X_test)
-mlp_probas = nb.fit(X_train, y_train).predict_proba(X_test)
+    rf_probas = rf.fit(X_train, y_train).predict_proba(X_test)
+    ada_probas = ada.fit(X_train, y_train).predict_proba(X_test)
+    lr_probas = lr.fit(X_train, y_train).predict_proba(X_test)
+    nb_probas = nb.fit(X_train, y_train).predict_proba(X_test)
+    svm_scores = svm.fit(X_train, y_train).decision_function(X_test)
+    mlp_probas = nb.fit(X_train, y_train).predict_proba(X_test)
 
-probas_list = [rf_probas, ada_probas, lr_probas, nb_probas, svm_scores, mlp_probas]
+    probas_list = [rf_probas, ada_probas, lr_probas, nb_probas, svm_scores, mlp_probas]
 
+    clf_names = ['Random Forest', 'Ada Boost', 'Logistic Regression',
+                 'Gaussian Naive Bayes', 'Support Vector Machine', 'Neural Network']
 
-clf_names = ['Random Forest', 'Ada Boost', 'Logistic Regression',
-             'Gaussian Naive Bayes', 'Support Vector Machine', 'Neural Network']
+    import scikitplot as skplt
+    
+    print('\n')
+    print('Calibration Curve:\n')
+    skplt.metrics.plot_calibration_curve(y_test,probas_list,clf_names)
+    plt.show()
+    
+    # Plotting classification reports
+    print('\n')
+    print('Classification report for Random Forest\n',
+          classification_report(y_test, rf.fit(X_train, y_train).predict(X_test)))
+    skplt.metrics.plot_confusion_matrix(y_test, rf.fit(X_train, y_train).predict(X_test), normalize=False,
+                                        title='Random Forest')
+    plt.show()
 
-skplt.metrics.plot_calibration_curve(y_test,probas_list,clf_names)
-skplt.metrics.plot_confusion_matrix(y_test, ada.fit(X_train, y_train).predict(X_test), normalize=False)
+    print('\n')
+    print('Classification report for AdaBoost\n',
+          classification_report(y_test, mlp.fit(X_train, y_train).predict(X_test)))
+    skplt.metrics.plot_confusion_matrix(y_test, ada.fit(X_train, y_train).predict(X_test), normalize=False,
+                                        title='AdaBoost')
+    plt.show()
+
+    print('\n')
+    print('Classification report for Logistic Regression\n',
+          classification_report(y_test, lr.fit(X_train, y_train).predict(X_test)))
+    skplt.metrics.plot_confusion_matrix(y_test, lr.fit(X_train, y_train).predict(X_test), normalize=False,
+                                        title='Logistic Regression')
+    plt.show()
+
+    print('\n')
+    print('Classification report for Naive Bayes\n',
+          classification_report(y_test, nb.fit(X_train, y_train).predict(X_test)))
+    skplt.metrics.plot_confusion_matrix(y_test, lr.fit(X_train, y_train).predict(X_test), normalize=False,
+                                        title='Naive Bayes')
+    plt.show()
+
+    print('\n')
+    print('Classification report for SVClassifier\n',
+          classification_report(y_test, nb.fit(X_train, y_train).predict(X_test)))
+    skplt.metrics.plot_confusion_matrix(y_test, svm.fit(X_train, y_train).predict(X_test), normalize=False,
+                                        title='SVClassifier')
+    plt.show()
+
+    print('\n')
+    print('Classification report for Neural Network\n',
+          classification_report(y_test, mlp.fit(X_train, y_train).predict(X_test)))
+    skplt.metrics.plot_confusion_matrix(y_test, mlp.fit(X_train, y_train).predict(X_test), normalize=False,
+                                        title='Neural Network')
+    plt.show()
+
+# Display plots of all classifications
+plot_classifications()
